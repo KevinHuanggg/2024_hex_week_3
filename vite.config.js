8bc5eacf1f65230e9d33f892e4afa6d2ea581252
Kevin Huang <qw820301@gmail.com>
2024-07-25 18:43:14 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
+import { defineConfig } from "vite"
+import { ViteEjsPlugin } from "vite-plugin-ejs"
+import { fileURLToPath } from "node:url"
+import path from "node:path"
+import { glob } from "glob"
 
-import liveReload from 'vite-plugin-live-reload';
+import liveReload from "vite-plugin-live-reload"
 
 function moveOutputPlugin() {
   return {
-    name: 'move-output',
-    enforce: 'post',
-    apply: 'build',
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
     async generateBundle(options, bundle) {
       for (const fileName in bundle) {
-        if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
-          bundle[fileName].fileName = newFileName;
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length)
+          bundle[fileName].fileName = newFileName
         }
       }
     },
-  };
+  }
 }
 
 export default defineConfig({
